21d7743fcf61d3bf82ba9ca1c6439f9136344591
Amirjon Nazarov <a3465172@gmail.com>
2026-03-15 11:45:07 +0500

    some endpoints

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+fake_items = [
+    {
+        "item_name": "apple"
+    },
+    {
+        "item_name": "banana"
+    },
+    {
+        "item_name": "pineapple"
+    }
+]
+
 @app.get("/hello")
 def say_hello():
     return {"message":"hello"}
@@ -12,3 +24,19 @@ def main_menu():
 @app.get("/about")
 def about():
     return {"name":"amir"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q:str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+
+@app.get("/search")
+def search(name: str, age: int):
+    return {"name": name, "age": age}
+
+
+@app.get("/user/{user_id}")
+def get_user(user_id: int):
+    return {"user_id": user_id}
